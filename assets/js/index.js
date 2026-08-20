3fe3ef48f6aa4c75a8f5ac72a49ef6e2b60cb16b
kuchinaevgenia <kucinaevgenia6@gmail.com>
2026-08-20 17:40:41 +0300

    Complete task1

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+function pow(base, exponent) {
+  if (typeof base !== "number" || typeof exponent !== "number") {
+    throw new TypeError("params must be numbers");
+  }
+  if (exponent < 0 || !Number.isSafeInteger(exponent)) {
+    throw new RangeError("exponent must be positive and integer");
+  }
+  if (exponent === 0) {
+    return 1;
+  }
+  return base * pow(base, exponent - 1);
+}
+try {
+  console.log(pow(3, 3));
+} catch (err) {
+  if (err instanceof TypeError) {
+    console.log("TypeError handling");
+  } else if (err instanceof RangeError) {
+    console.log("RangeError handling");
+  } else if (err instanceof Error) {
+    console.log("Error handling");
+  }
+}
